d5142fb75898422e15ac68c4923c8ab651e1c586
n7560125 <95419045+n7560125@users.noreply.github.com>
2021-12-14 16:33:04 +0800

    add Bullet

---
+++ Assets/1110/Bullet.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float fSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * fSpeed * Time.deltaTime;
+    }
+    public void Fire(Vector3 start, Vector3 target)
+    {
+        gameObject.SetActive(true);
+        transform.position = start;
+        transform.forward = target - start;
+    }
+}
--- Assets/1110/FPScontroller.cs
+++ Assets/1110/FPScontroller.cs
@@ -6,6 +6,9 @@ public class FPScontroller : MonoBehaviour
 {
     public float fMoveMul;
     public float fControllerSensitivity;
+    public GameObject Bullet;
+    public GameObject EmitPoint;
+    public LayerMask hitlayers;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +18,27 @@ public class FPScontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Transform cT = Camera.main.transform;
+        //when mousebutton down(fire);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray r = new Ray(cT.position, cT.forward);
+            RaycastHit rh;
+            Vector3 vTarget = cT.position + cT.forward * 100.0f;
+            if(Physics.Raycast(r, out rh, 100.0f, hitlayers)) 
+            {
+                Vector3 vDir = rh.point - cT.position;
+                vTarget = rh.point;
+                float fD = vDir.magnitude;
+                if (fD < 10.0f)
+                {
+                    vTarget = cT.position + cT.forward * 10.0f;
+                }
+            }
+            Bullet b = Bullet.GetComponent<Bullet>();
+            //Bullet.transform.pos ition = EmitPoint.transform.position;
+            b.Fire(EmitPoint.transform.position, vTarget);
+        }
         float fMX = Input.GetAxis("Mouse X")*fControllerSensitivity;
         float fMY = Input.GetAxis("Mouse Y")*fControllerSensitivity;
         transform.Rotate(0.0f, fMX, 0.0f);
